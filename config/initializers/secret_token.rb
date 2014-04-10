bcc226f1e17dc0b3861b72c8796d7ff43e086f78
liz tom & Jason Hamilton <student@epicodus.com>
2014-04-10 10:01:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlbumShare::Application.config.secret_key_base = 'ba7b11082f86406b22425197551930d567269104f12936e8396aa9ad07441187a0ad3c0d68c5bf09b0f676ed3a2f74dc967cd8e1f8783c825a4cc4cb7b63719c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlbumShare::Application.config.session_store :cookie_store, key: '_album-share_session'
